698011c527a8a1db59485599141baa91ff787e87
Carlos Santana <csantanapr@apache.org>
2019-05-01 23:58:56 -0400

    refactor for zipkin

--- app-rates/app.js
+++ app-rates/app.js
@@ -7,13 +7,12 @@ var indexRouter = require('./routes/index');
 var ratesRouter = require('./routes/rates/rates');
 
 var tracingMiddleware = require("./lib/tracing-middleware");
-var tracer = require('./lib/tracing')('rates-service')
-var opentracing = require("opentracing")
+
+
 
 var app = express();
 
-// initialize tracing into expressjs
-opentracing.initGlobalTracer(tracer);
+
 app.use(tracingMiddleware());
 
 app.use(logger('dev'));
--- app-rates/lib/tracing-middleware.js
+++ app-rates/lib/tracing-middleware.js
@@ -1,16 +1,29 @@
 // original code from https://github.com/opentracing-contrib/javascript-express/blob/master/src/middleware.js
 
-var opentracing = require("opentracing");
+var initTracer = require('./tracing')
+var opentracing = require("opentracing")
+var ZipkinB3TextMapCodec = require("jaeger-client").ZipkinB3TextMapCodec;
 var url = require("url");
 
-function middleware(options = {}) {
-    const tracer = options.tracer || opentracing.globalTracer();
+let serviceName = process.env.TRACER_SERVICE_NAME || "rates-service"
+var tracer = initTracer(serviceName)
+// initialize tracing into expressjs
+opentracing.initGlobalTracer(tracer);
 
+if (process.env.ZIPKIN_HEADERS) {
+    // using zipkin headers
+    console.debug('request contains zipkin headers')
+    var codec = new ZipkinB3TextMapCodec({ urlEncoding: true });
+    tracer.registerInjector(opentracing.FORMAT_HTTP_HEADERS, codec);
+    tracer.registerExtractor(opentracing.FORMAT_HTTP_HEADERS, codec);
+}
+function middleware(options = {}) {
     return (req, res, next) => {
         const wireCtx = tracer.extract(opentracing.FORMAT_HTTP_HEADERS, req.headers);
         const pathname = url.parse(req.url).pathname;
         const span = tracer.startSpan(pathname, { childOf: wireCtx });
         span.log({ 'event': 'request_received' });
+        span.log(req.headers)
 
         // include some useful tags on the trace
         span.setTag(opentracing.Tags.HTTP_METHOD, req.method);
