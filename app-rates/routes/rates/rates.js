da5261e3be05dd91ce7474b22b06dacfaf281725
Carlos Santana <csantanapr@apache.org>
2019-05-10 17:13:55 -0400

    add hostname to return and support all methods

--- app-rates/routes/rates/rates.js
+++ app-rates/routes/rates/rates.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var ratesDB = require('./ratesFromDB');
 
-router.get('/', function (req, res, next) {
+const hostname = process.env.HOSTNAME || 'localhost';
+
+requestHandler = function (req, res, next) {
     // show how to do a Tag KV
     let memberid = req.query.memberid || "0000";
     const ctx = req;
@@ -13,11 +15,14 @@ router.get('/', function (req, res, next) {
     ctx.span.setBaggageItem("memberid", memberid);
     // show how to pass down the span parent context
     ratesDB.getRates(ctx).then((rates) => {
+        rates.hostname = hostname;
         res.json(rates);
         next()
     }).catch((err) => {
         res.status(500).send({ error: err.message });
     });
-});
+}
+
+router.use('/', requestHandler);
 
 module.exports = router;
